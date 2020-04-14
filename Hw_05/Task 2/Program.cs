5346e62e0c18841e7a0eff63ae0badd8506c4ad1
Unknown <jobbazjob@gmail.com>
2020-04-14 11:45:19 +0300

    Hw_05

---
+++ Hw_05/Task 2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void mapRender(char[,] map) //Очищает и рендерит карту;
+        {
+            Console.Clear();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 7; j++)
+                {
+                    Console.Write(map[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int heroX = 3; 
+            int heroY = 3;
+
+            char[,] map =
+            {
+                {'#','#','#','#','#','#','#'},
+                {'#','.','.','.','.','.','#'},
+                {'#','.','.','.','.','.','#'},
+                {'#','.','.','.','.','.','#'},
+                {'#','#','#','#','#','#','#'}
+            };
+
+            map[heroY, heroX] = '@'; 
+
+            mapRender(map); 
+
+            ConsoleKeyInfo keypress;
+            do
+            {
+                keypress = Console.ReadKey();
+
+                if (keypress.KeyChar == 'w') 
+                {
+                    if (map[heroY - 1, heroX] != '#') 
+                    {
+                        map[heroY, heroX] = '.';
+                        map[heroY - 1, heroX] = '@';
+                        heroY--;
+                        mapRender(map);
+                    }
+                    else mapRender(map);
+                }
+
+                if (keypress.KeyChar == 'd') 
+                {
+                    if (map[heroY, heroX + 1] != '#')
+                    {
+                        map[heroY, heroX] = '.';
+                        map[heroY, heroX + 1] = '@';
+                        heroX++;
+                        mapRender(map);
+                    }
+                    else mapRender(map);
+                }
+
+                if (keypress.KeyChar == 's')  
+                {
+                    if (map[heroY + 1, heroX] != '#')
+                    {
+                        map[heroY, heroX] = '.';
+                        map[heroY + 1, heroX] = '@';
+                        heroY++;
+                        mapRender(map);
+                    }
+                    else mapRender(map);
+                }
+
+                if (keypress.KeyChar == 'a')  
+                {
+                    if (map[heroY, heroX - 1] != '#')
+                    {
+                        map[heroY, heroX] = '.';
+                        map[heroY, heroX - 1] = '@';
+                        heroX--;
+                        mapRender(map);
+                    }
+                    else mapRender(map);
+                }
+
+            } while (keypress.KeyChar != 'Q');
+        }
+    }
+}
---
+++ Hw_05/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hm_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "abcdefghijklmnopqrstuvwxyz";
+
+            for (int i = 25; i < letters.Length; i--)
+                Console.WriteLine(letters[i]);
+        }
+    }
+}
